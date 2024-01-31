3052da4726696afff9350ba4d75a4cc2ee230df0
Rafiul Asif <48728125+rafi456131@users.noreply.github.com>
2024-01-31 20:47:10 +0000

    finish blog project

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,17 +1,36 @@
+import { useQuery } from "@tanstack/react-query";
 import { fetchComments } from "./api";
 import "./PostDetail.css";
 
-export function PostDetail({ post }) {
+export function PostDetail({ post, deleteMutation, updateMutation }) {
   // replace with useQuery
-  const data = [];
+  const { data, isLoading } = useQuery({
+    queryKey: ["comments", post.id],
+    queryFn: () => fetchComments(post.id),
+  });
 
+  if (isLoading) return <p>loading</p>;
   return (
     <>
       <h3 style={{ color: "blue" }}>{post.title}</h3>
-      <button>Delete</button> <button>Update title</button>
+      <div>
+        <button onClick={() => deleteMutation.mutate(post.id)}>Delete</button>
+        {deleteMutation.isPending && (
+          <p className="loading">Deleting the post</p>
+        )}
+        {deleteMutation.isSuccess && (
+          <p className="success">The post is deleted</p>
+        )}
+      </div>
+      <div>
+        <button onClick={() => updateMutation.mutate(post.id)}>
+          Update title
+        </button>
+        {updateMutation.isSuccess && <p className="success">post is updated</p>}
+      </div>
       <p>{post.body}</p>
       <h4>Comments</h4>
-      {data.map((comment) => (
+      {data?.map((comment) => (
         <li key={comment.id}>
           {comment.email}: {comment.body}
         </li>
--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,40 +1,83 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import { fetchPosts, deletePost, updatePost } from "./api";
 import { PostDetail } from "./PostDetail";
+import { useQuery, useQueryClient, useMutation } from "@tanstack/react-query";
 const maxPostPage = 10;
 
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
+  const queryClient = useQueryClient();
+  useEffect(() => {
+    if (currentPage < maxPostPage) {
+      const nextPage = currentPage + 1;
+      queryClient.prefetchQuery({
+        queryKey: ["posts", nextPage],
+        queryFn: () => fetchPosts(nextPage),
+      });
+    }
+  }, [currentPage, queryClient]);
+
+  const deleteMutation = useMutation({
+    mutationFn: (postId) => deletePost(postId),
+  });
+
+  const updateMutation = useMutation({
+    mutationFn: (postId) => updatePost(postId),
+  });
 
   // replace with useQuery
-  const data = [];
-
+  const { data, isLoading } = useQuery({
+    queryKey: ["posts", currentPage],
+    queryFn: () => fetchPosts(currentPage),
+    staleTime: 2000, //2s
+  });
+  if (isLoading) return <p>Still Loading.....</p>;
   return (
     <>
       <ul>
-        {data.map((post) => (
+        {data?.map((post) => (
           <li
             key={post.id}
             className="post-title"
-            onClick={() => setSelectedPost(post)}
+            onClick={() => {
+              setSelectedPost(post);
+              deleteMutation.reset();
+              updateMutation.reset();
+            }}
           >
             {post.title}
           </li>
         ))}
       </ul>
       <div className="pages">
-        <button disabled onClick={() => {}}>
+        <button
+          disabled={currentPage <= 1}
+          onClick={() => {
+            setCurrentPage((s) => s - 1);
+          }}
+        >
           Previous page
         </button>
-        <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
+        <span>Page {currentPage}</span>
+        <button
+          disabled={currentPage >= maxPostPage}
+          onClick={() => {
+            setCurrentPage((s) => s + 1);
+          }}
+        >
           Next page
         </button>
       </div>
       <hr />
-      {selectedPost && <PostDetail post={selectedPost} />}
+      {selectedPost && (
+        <PostDetail
+          post={selectedPost}
+          deleteMutation={deleteMutation}
+          updateMutation={updateMutation}
+        />
+      )}
     </>
   );
 }
